0dc21e286c5b8fa04b0ccb3b33d6dc6978a2ed58
vic778 <victoiremmanuelbarh@gmail.com>
2024-12-07 15:33:46 +0400

    extract the metadata

--- app/models/dog_profile.rb
+++ app/models/dog_profile.rb
@@ -1,5 +1,19 @@
 class DogProfile < ApplicationRecord
   has_one_attached :photo
+  validates :photo, attached: true, content_type: [ "image/png", "image/jpg", "image/jpeg" ]
 
-  validates :photo, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  after_commit :extract_photo_metadata, on: :create
+
+  private
+
+  def extract_photo_metadata
+    return unless photo.attached?
+
+    photo.open do |file|
+      dimensions = FastImage.size(file.path)
+      format = FastImage.type(file.path)
+      update(metadata: { width: dimensions[0], height: dimensions[1], format: format, size: photo.byte_size })
+    end
+  end
+
 end
