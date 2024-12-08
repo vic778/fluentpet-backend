c5651154eb190c9429a674622db80b60e8bdfb96
vic778 <victoiremmanuelbarh@gmail.com>
2024-12-08 16:27:20 +0400

    Custoize data for caching

--- app/helpers/audio_files_helper.rb
+++ app/helpers/audio_files_helper.rb
@@ -1,2 +1,20 @@
 module AudioFilesHelper
+  def hashed_data(button_events)
+    button_events.map do |button_event|
+      {
+        id: button_event.id,
+        button_id: button_event.button_id,
+        pressed_on: button_event.timestamp.strftime("%m/%d/%Y %H:%M:%S"),
+        audio_files: button_event.audio_files.map do |audio_file|
+          {
+            id: audio_file.id,
+            button_event_id: audio_file.button_event_id,
+            registrested_on: audio_file.timestamp.strftime("%m/%d/%Y %H:%M:%S"),
+            duration: audio_file.duration,
+            file: url_for(audio_file.file)
+          }
+        end
+      }
+    end
+  end
 end
