77c3026a55de990f4dfd05d2444d5994bd1dcfc9
vic778 <victoiremmanuelbarh@gmail.com>
2024-12-08 12:50:41 +0400

    Display god events include the audios

--- app/controllers/audio_files_controller.rb
+++ app/controllers/audio_files_controller.rb
@@ -1,4 +1,19 @@
 class AudioFilesController < ApplicationController
+  def index
+    dog_profile_id = params[:dog_profile_id]
+
+    if dog_profile_id.blank?
+      render json: { error: "dog_profile_id is required" }, status: :bad_request
+      return
+    end
+
+    button_events = ButtonEvent.includes(:audio_files)
+                               .where(dog_profile_id: dog_profile_id)
+                               .order(created_at: :desc)
+
+    render template: "audio_files/index", locals: { button_events: button_events }
+  end
+
   def create
     audio_file = AudioFile.new(audio_file_params)
 
@@ -6,42 +21,15 @@ class AudioFilesController < ApplicationController
       ProcessAudioJob.perform_later(audio_file.id)
       file_url = url_for(audio_file.file)
 
-      save_to_dynamodb(audio_file, file_url)
-
       render json: { id: audio_file.id, file_url: file_url }, status: :created
     else
       render json: { errors: audio_file.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  def index
-    button_id = params[:button_id]
-
-    if button_id.blank?
-      render json: { error: "button_id is required" }, status: :bad_request
-      return
-    end
-
-    dynamo_service = DynamoDbService.new
-    audio_files = dynamo_service.get_audio_files(button_id)
-
-    render json: { button_id: button_id, audio_files: audio_files }, status: :ok
-  end
-
   private
 
   def audio_file_params
-    params.require(:audio_file).permit(:button_id, :timestamp, :duration, :file)
-  end
-
-  def save_to_dynamodb(audio_file, file_url)
-    dynamo_service = DynamoDbService.new
-
-    dynamo_service.save_audio_file(
-      button_id: audio_file.button_id,
-      timestamp: audio_file.timestamp.to_s,
-      audio_url: file_url,
-      duration: audio_file.duration
-    )
+    params.require(:audio_file).permit(:timestamp, :duration, :file, :button_event_id)
   end
 end
